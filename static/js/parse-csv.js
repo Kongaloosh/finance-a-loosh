951f75a675cbf0ef28de3453901ea9250332f1c4
Alex Kearney <hi@alexkearney.com>
2022-01-28 17:18:18 -0700

    parses csv and strips header if it exists.

--- static/js/parse-csv.js
+++ static/js/parse-csv.js
@@ -1,13 +1,46 @@
 $(document).ready(function(){
+    function printTable(file) {
+        var html = '';
+        for(var row in data) {
+          html += '<tr>\r\n';
+          for(var item in data[row]) {
+            html += '<td>' + data[row][item] + '</td>\r\n';
+          }
+          html += '</tr>\r\n';
+        }
+        $('#placeholder').html(html);
+      };
+
+    function isHeader(header){
+        var isFloatPresent = false;
+        for (let i = 0; i < header.length; i++){
+            if (!Number.isNaN(parseFloat(header[i]))){
+                isFloatPresent = true;
+            }
+        }
+        if (isFloatPresent){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
     document.getElementById('file-selector').addEventListener('change', event => {
         file = event.target.files[0];
         const reader = new FileReader();
         reader.addEventListener('load', (event) => {
-            console.log(event.target.result);
-            document.getElementById('placeholder').value = event.target.result;
+            var result = $.csv.toArrays(event.target.result);
+            document.getElementById('placeholder').value = result;
+            var headerLine = isHeader(result[0]);
+            // strip the header if it exists; we will look for values implicitly
+            if (headerLine){
+                result.shift()
+            }
+            console.log(result)
         }, false);
         if(file){
-            reader.readAsDataURL(file);
+            reader.readAsText(file);
         }
+
     });
 });
